5df66eacc4ff46490034ff2fe22f6c7880b3435a
Pierrosin <pierrickdossin@live.fr>
2021-08-06 16:27:44 +0200

    enter exit et step elevator

--- elevator.c
+++ elevator.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "elevator.h"
-
-
-
-
-
+#include "person.h"
 
 // typedef struct _Building
 // {
@@ -14,10 +10,9 @@
 //     PersonList **waitingLists; // array of waiting list (one per floor)
 // } Building;
 
-
-
-Elevator * create_elevator(int capacity, int currentFloor, PersonList * persons){
-    Elevator * E = malloc(sizeof(Elevator));
+Elevator *create_elevator(int capacity, int currentFloor, PersonList *persons)
+{
+    Elevator *E = malloc(sizeof(Elevator));
     E->capacity = capacity;
     E->currentFloor = currentFloor;
     E->targetFloor = currentFloor;
@@ -25,19 +20,79 @@ Elevator * create_elevator(int capacity, int currentFloor, PersonList * persons)
     return E;
 }
 
-
-
-Building * create_building(int nbFloor, Elevator *elevator, PersonList **waitingLists){
-    Building * B = malloc( sizeof( Building ) );
+Building *create_building(int nbFloor, Elevator *elevator, PersonList **waitingLists)
+{
+    Building *B = malloc(sizeof(Building));
     B->nbFloor = nbFloor;
     B->elevator = elevator;
     B->waitingLists = waitingLists;
     return B;
 }
-PersonList * exitElevator(Elevator *e){
-    //pour chaque personne dans l'ascenseur, on prend celles qui veulent descendre
-    //(dest == currentFloor) et on les supprime de la PersonList
-    //on change ensuite le nombre de personnes dans l'ascenseur
+
+PersonList *exitElevator(Elevator *e)
+{
+    PersonList *L = NULL;
+    PersonList *person_list = e->persons;
+    int floor = e->currentFloor;
+    while (person_list)
+    {
+        if (person_list->head->dest != floor)
+        {
+            L = insert(person_list->head, L);
+        }
+        person_list = person_list->tail;
+    }
+    e->persons = L;
+    return L;
 }
-PersonList * enterElevator(Elevator *e, PersonList *list);
-void stepElevator(Building *b);
+
+PersonList *enterElevator(Elevator *e, PersonList *list)
+{
+    PersonList *person_list = e->persons;
+    int capacity = e->capacity;
+    int floor = e->currentFloor;
+    while (len(e->persons) < capacity && list)
+    {
+        insert_end(list->head, e->persons);
+        list = list->tail;
+    }
+    return list;
+}
+
+void stepElevator(Building *b)
+{
+    int curFloor = b->elevator->currentFloor;
+    int tarFloor = b->elevator->targetFloor;
+    if (curFloor == tarFloor)
+    {
+        exitElevator(b->elevator);
+        PersonList *new_waiting_list = enterElevator(b->elevator, b->waitingLists[curFloor]);
+        b->waitingLists[curFloor] = new_waiting_list;
+    }
+    else
+    {
+        if (curFloor < tarFloor)
+        {
+            b->elevator->currentFloor++;
+        }
+        else
+        {
+            b->elevator->currentFloor--;
+        }
+    }
+}
+
+// void main()
+// {
+//     Elevator *e = create_elevator(3, 0, create_PersonList(create_Person(0, 0)));
+//     Person *p = create_Person(3, 0);
+//     Person *q = create_Person(0, 2);
+//     PersonList *personlist = create_PersonList(p);
+//     insert_end(q, personlist);
+//     insert_end(q, personlist);
+//     PersonList *L = enterElevator(e, personlist);
+//     print_PersonList(L);
+//     print_PersonList(e->persons);
+//     L = exitElevator(e);
+//     print_PersonList(L);
+// }
--- person.c
+++ person.c
@@ -2,53 +2,59 @@
 #include <stdlib.h>
 #include "person.h"
 
-
-Person * create_Person( int src , int dest ) {
-    Person * p = malloc( sizeof( Person ) );
+Person *create_Person(int src, int dest)
+{
+    Person *p = malloc(sizeof(Person));
     p->src = src;
     p->dest = dest;
-    return( p );
-
+    return (p);
 }
 
-void print_Person( Person * p ) {
-    printf( "%d%d" , p->src , p->dest );
+void print_Person(Person *p)
+{
+    printf("%d%d", p->src, p->dest);
 }
 
-PersonList * create_PersonList( Person * p ) {
-    PersonList * l = malloc( sizeof( PersonList ) );
+PersonList *create_PersonList(Person *p)
+{
+    PersonList *l = malloc(sizeof(PersonList));
     l->head = p;
     l->tail = NULL;
-    return( l );
-
+    return (l);
 }
 
-
-
-PersonList * insert( Person * p , PersonList * plist ) {
-    PersonList * L = create_PersonList( p );
+PersonList *insert(Person *p, PersonList *plist)
+{
+    PersonList *L = create_PersonList(p);
     L->tail = plist;
     return L;
 }
 
-void print_PersonList( PersonList * liste ) {
-    PersonList * liste_tmp = liste;
-    printf( "[ " );
-    while ( liste_tmp ) {
-        print_Person( liste_tmp->head );
-        printf( " " );
+void print_PersonList(PersonList *liste)
+{
+    PersonList *liste_tmp = liste;
+    printf("[ ");
+    while (liste_tmp)
+    {
+        print_Person(liste_tmp->head);
+        printf(" ");
         liste_tmp = liste_tmp->tail;
     }
-    printf( "]\n" );
+    printf("]\n");
 }
 
-void insert_end( Person * p , PersonList * L ) {
-    if ( !L ) {
-        L = create_PersonList( p );
-    } else {
-        PersonList * l = create_PersonList( p );
-        PersonList * liste_tmp = L;
-        while ( liste_tmp->tail ) {
+void insert_end(Person *p, PersonList *L)
+{
+    if (!L)
+    {
+        L = create_PersonList(p);
+    }
+    else
+    {
+        PersonList *l = create_PersonList(p);
+        PersonList *liste_tmp = L;
+        while (liste_tmp->tail)
+        {
 
             liste_tmp = liste_tmp->tail;
         }
@@ -56,37 +62,55 @@ void insert_end( Person * p , PersonList * L ) {
     }
 }
 
-int egalPerson( Person * p1 , Person * p1 ) {
-    return( p1->dest == p2->dest && p1->src == p2->src );
+int egalPerson(Person *p1, Person *p2)
+{
+    return (p1->dest == p2->dest && p1->src == p2->src);
 }
 
-PersonList * removePerson( Person * p , PersonList * L ) {
+int len(PersonList *L)
+{
+    int len = 0;
+    PersonList *liste_tmp = L;
+    while (liste_tmp)
+    {
+        len++;
+        liste_tmp = liste_tmp->tail;
+    }
+    return len;
+}
+
+PersonList *removePerson(Person *p, PersonList *L)
+{
     // on crée une liste dans laquelle on ajoute toutes les personnes dans L qui ne sont pas p
-    PersonList * l = NULL;
-    PersonList * liste_tmp = L;
-    while ( liste_tmp ) {
-        Person * Head = liste_tmp->head;
-        if ( !egalPerson( Head , p ) ) {
-            insert_end( Head , l );
+    PersonList *l = NULL;
+    PersonList *liste_tmp = L;
+    while (liste_tmp)
+    {
+        Person *Head = liste_tmp->head;
+        if (!egalPerson(Head, p))
+        {
+            insert_end(Head, l);
         }
     }
     return l;
-
 }
 
 // //JEUX DE TEST
 
-// void main( ) {
-//     Person * p = create_Person( 3 , 4 );
-//     Person * q = create_Person( 1 , 2 );
-//     Person * r = create_Person( 5 , 6 );
-//     print_Person( p );
-//     printf( "\n" );
-//     PersonList * L = create_PersonList( p );
-//     print_PersonList( L );
-//     L = insert( q , L );
-//     print_PersonList( L );
-//     insert_end( r , L );
-//     print_PersonList( L );
-
+// void main()
+// {
+//     Person *p = create_Person(3, 4);
+//     Person *q = create_Person(1, 2);
+//     Person *r = create_Person(5, 6);
+//     print_Person(p);
+//     printf("\n");
+//     PersonList *L = create_PersonList(p);
+//     print_PersonList(L);
+//     L = insert(q, L);
+//     print_PersonList(L);
+//     insert_end(r, L);
+//     print_PersonList(L);
+//     L = NULL;
+//     L = insert(p, L);
+//     print_PersonList(L);
 // }
